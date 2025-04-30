e46b19ca51a3a18c9e9133a3b8a6e5f35ab3ae5d
uchechukwu <uchechukwupeter22@gmail.com>
2025-04-30 14:47:55 +0100

    refactor(Home): comment out AOS import statement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
 // react libraries
-import AOS from "aos";
+// import AOS from "aos";
 import "aos/dist/aos.css";
 
 // react-icons
